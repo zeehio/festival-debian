ad57b72859331c6f3b271840a1a3b64fa11be9c2
Kumar Appaiah <akumar@ee.iitm.ac.in>
2007-11-27 10:38:42 +0530

    Import debian/1.96-beta-3
    Signed-off-by: Kumar Appaiah <akumar@ee.iitm.ac.in>

---
+++ debian/festival.scm
@@ -0,0 +1,3 @@
+;; Any site-wide Festival initialization can be added to this file.
+;; It is marked as a configuration file, so your changes will be saved
+;; across upgrades of the Festival package.
