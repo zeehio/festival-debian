9d1d322336eb65eecf1644b1f2a6fc49f4a2e744
Kumar Appaiah <akumar@ee.iitm.ac.in>
2008-03-09 14:02:55 +0530

    Remove festival server startup.

--- debian/festival.scm
+++ debian/festival.scm
@@ -1,6 +1,13 @@
-;; Any site-wide Festival initialization can be added to this file.
-;; It is marked as a configuration file, so your changes will be saved
-;; across upgrades of the Festival package.
+;; WARNING: It is inherently insecure to run a festival instance as a
+;; server, mainly because it exposes the whole system to exploits which
+;; can be easily used by attackers to gain access to your
+;; computer. This is because of the inherent design of the festival
+;; server. Please use it only in a situation where you are sure that
+;; you will not be subjected to such an attack, or have adequate
+;; security precautions.
+
+;; This file has been provided as an example file for your use, should
+;; you wish to run festival as a server.
 
 ; Maximum number of clients on the server
 (set! server_max_clients 10)
@@ -8,8 +15,8 @@
 ; Server port
 (set! server_port 1314)
 
-; Server password
-(set! server_passwd nil)
+; Server password:
+(set! server_passwd "password")
 
 ; Log file location
 (set! server_log_file "/var/log/festival/festival.log")
